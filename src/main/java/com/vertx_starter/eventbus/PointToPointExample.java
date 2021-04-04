60470bbcf6f9a50cc7191437e7b50a5e7234e55b
edevarfilho@gmail.com <edevarfilho@gmail.com>
2021-04-04 10:49:34 -0300

    Sending a message with Sender, don't wait response

---
+++ src/main/java/com/vertx_starter/eventbus/PointToPointExample.java
@@ -0,0 +1,36 @@
+package com.vertx_starter.eventbus;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class PointToPointExample {
+
+  public static void main(String[] args) {
+    var vertx = Vertx.vertx();
+    vertx.deployVerticle(new Sender());
+    vertx.deployVerticle(new Receiver());
+  }
+
+  static class Sender extends AbstractVerticle{
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      //send a message each 1s
+      vertx.setPeriodic(1000, id -> {
+        vertx.eventBus().send(Sender.class.getName(), "Sending a message without waiting for response");
+      });
+    }
+  }
+
+  static class Receiver extends AbstractVerticle{
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.eventBus().consumer(Sender.class.getName(), message -> {
+        System.out.println("Received the message: " + message.body());
+      });
+    }
+  }
+
+}
