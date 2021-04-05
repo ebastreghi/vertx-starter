fb68cc284c635d240beebd1ca323c71d53ffe6e1
edevarfilho@gmail.com <edevarfilho@gmail.com>
2021-04-05 16:44:15 -0300

    Future coordination and map

--- src/test/java/com/vertx_starter/FuturePromiseExample.java
+++ src/test/java/com/vertx_starter/FuturePromiseExample.java
@@ -3,6 +3,8 @@ package com.vertx_starter;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.Test;
@@ -40,6 +42,47 @@ public class FuturePromiseExample {
       context.completeNow();
       })
       .onFailure(context::failNow);
+  }
+
+  @Test
+  void future_map(Vertx vertx, VertxTestContext context){
+    final Promise<String> promise = Promise.promise();
+    System.out.println("Start");
+    vertx.setTimer(5000, id -> {
+      promise.complete("Success");
+      System.out.println("Timer done");
+    });
+    final Future<String> future = promise.future();
+    future
+      .map(asString -> {
+        System.out.println("Map String to JsonObject");
+        return new JsonObject().put("key", asString);
+      })
+      .map(jsonObject -> new JsonArray().add(jsonObject))
+      .onSuccess(result -> {
+        System.out.println("Result: " + result);
+        context.completeNow();
+      })
+      .onFailure(context::failNow);
+  }
+
+  @Test
+  void future_coordination(Vertx vertx, VertxTestContext context){
+    vertx.createHttpServer()
+      .requestHandler(request -> System.out.println(request))
+      .listen(10000)
+      .compose(server -> {
+        System.out.println("Another task");
+        return Future.succeededFuture(server);
+      })
+      .compose(server -> {
+        System.out.println("Even more");
+        return Future.succeededFuture(server);
+      })
+      .onFailure(context::failNow)
+      .onSuccess(server -> {
+        System.out.println("Server started on port " + server.actualPort());
+      });
   }
 
 }
